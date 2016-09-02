7a019e698e4c68361cc33bb06bbe7a6e9e86720b
cgrah064 <cgrah064@uottawa.ca>
2016-09-01 22:19:57 -0700

    Create fishJava.js

---
+++ fishJava.js
@@ -0,0 +1,17 @@
+window.curQuery = "SELECT * FROM test" //+$('#dbTable option:selected').text()
+
+$(document).ready(function(){
+  $("#getData").click(function(){ 
+    $.ajax({
+      type: "post",
+      url: "http://172.16.0.15/test.py/samplePyFunction",
+      data: {
+        "tableName": "test", //$('#dbTable option:selected').text(),
+        "currentQuery": curQuery //.join(',')	//if you want to pass a list, join it with a comma and use var.split(',') on the server side
+      },
+      dataType: "text"
+    }).done(function(result){
+      $("#querySpan").html(result); //this replaces an html element with your string result
+    });
+  });
+});
