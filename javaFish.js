1d7fad33d8521b4bc3096526c4e96b8037f455f2
cgrah064 <cgrah064@uottawa.ca>
2016-11-16 16:40:44 -0500

    Update javaFish.js

--- javaFish.js
+++ javaFish.js
@@ -24,11 +24,11 @@ $(document).ready(function(){
         $("#plantOpts").toggle();
     });
 });
-/*
+
 $(document).ready(function(){
     $(".opts").hide();
 });
-*/
+
 $(document).ready(function(){
   if (pathname.includes("cameraView")) {
     $.ajax({
